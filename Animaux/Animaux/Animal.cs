71232a1885030b94097e8a8724856cec2759fe4f
LERMECHAIN <laura.ledoux@viacesi.fr>
2024-03-26 10:46:16 +0100

    build : methode move et sleep

---
+++ Animaux/Animaux/Animal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animaux
+{
+    class Animal
+    {
+        public string Name;
+
+        public Animal()
+        {
+            // constructeur vide pour creer un animal sans nom (dsans attribut)
+        }
+        public Animal(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public void Move()
+        {
+            Console.WriteLine("Move");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("Sleep");
+        }
+    }
+}
--- Animaux/Animaux/Program.cs
+++ Animaux/Animaux/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Animaux;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal monChat = new Animal("Chopper");
+        monChat.Move();
+        monChat.Sleep();
+        Console.WriteLine(monChat.Name);
+    }
+}
